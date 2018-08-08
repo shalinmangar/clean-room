fc4fbcfe3651ed807ea295f33fd119183ace5e2f
Shalin Shekhar Mangar <shalin@apache.org>
2018-08-08 13:13:01 +0530

    Fixed run_command to properly log command output

--- src/python/utils.py
+++ src/python/utils.py
@@ -16,18 +16,18 @@ def run_get_output(command):
 
 
 def run_command(command, logger=logging.getLogger()):
-    from StringIO import StringIO
     logger.info('RUN: %s' % command)
     t0 = time.time()
     try:
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        output, error = process.communicate()
-
-        logger.info(StringIO(output))
-        logger.error(StringIO(error))
+        output, _ = process.communicate()
+        logger.info(output)
     except (OSError, subprocess.CalledProcessError) as exception:
         logger.error('Exception occurred: ' + str(exception))
         logger.error('Subprocess failed')
+        raise exception
+    except Exception as exception:
+        raise exception
     else:
         # no exception was raised
         logger.info('Subprocess finished')
